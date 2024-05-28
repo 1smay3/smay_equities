6e7acb80a2b6c52249cf8f2c67de122a021b6de5
1spencermay3 <spencermayy@gmail.com>
2024-05-28 21:21:32 +0100

    MAYC-4 Resolve merge conflicts

--- data/data_cache/arctic.py
+++ data/data_cache/arctic.py
@@ -1,18 +1,16 @@
-import arcticdb as adb
 import polars as pl
-from data.data_cache.definitions import LOCAL_ARCTIC_PATH
+import arcticdb as adb
 
+class ArcticHandler:
+    def __init__(self, local_arctic_path: str):
+        self.local_arctic_path = local_arctic_path
+        self.arctic_instance = adb.Arctic(f"lmdb:///{self.local_arctic_path}?map_size=20GB")
 
-def save_dataframe_to_arctic(arctic_library_name: str, data: pl.DataFrame, symbol: str):
-    arctic_instance = adb.Arctic(f"lmdb:///{LOCAL_ARCTIC_PATH}?map_size=20GB")
-    arctic_library = arctic_instance[arctic_library_name]
+    def create_arctic_library(self, library_name: str):
+        self.arctic_instance.create_library(library_name)
 
-    # Note: Converting straight into pandas for now, as Arctic doesnt
-    # support polars yet.
+    def save_dataframe_to_arctic(self, arctic_library_name: str, data: pl.DataFrame, symbol: str):
+        arctic_library = self.arctic_instance[arctic_library_name]
 
-    arctic_library.write(symbol, data.to_pandas())
-
-
-def _create_arctic_library(local_arctic_path: str, library_name):
-    ac = adb.Arctic(f"lmdb:///{local_arctic_path}?map_size=20GB")
-    ac.create_library(library_name)
+        # Note: Converting straight into pandas for now, as Arctic doesn't support Polars yet.
+        arctic_library.write(symbol, data.to_pandas())
--- get_data.py
+++ get_data.py
@@ -1,90 +1,35 @@
 """Click-ified script to gather and data_cache data from data API"""
 
 import logging
-
 import click
-
-from data.schedule import (
-    data_name_to_endpoint_mapping,
-    index_level_data_mapping,
-    index_level_symbol_mapping,
-    map_field_name_to_function,
-    single_stock_data_mapping,
-)
 from fmp import fmp_data
-from data.data_cache.arctic import save_dataframe_to_arctic
+from data.data_cache.definitions import LOCAL_ARCTIC_PATH
+from data.data_cache.arctic import ArcticHandler
 
 COUNT_DEBUG_NUMBER_OF_SYMBOLS = 10
 
 logger = logging.getLogger(__name__)
 
+arctic_store = ArcticHandler(LOCAL_ARCTIC_PATH)
 
-def get_single_stock_data():
-    """
-    For the parsed type and exchange (from our config), gather the relevant data
-    and data_cache in a format accessible for research (dates x stocks)
-    :param data_save_path: str
-        The path to use when pickling
-    :return: None
-    """
-    # Run for all elements of the schedule
-    for index_name, field_name in single_stock_data_mapping.items():
-        stock_index_endpoint_name = data_name_to_endpoint_mapping[index_name]
-
-        # TODO this should be a list of strings
-
-        remote_symbols_from_index = fmp_data.gather_symbols_from_index_no_survivorship(
-            stock_index_endpoint_name
-        )[:COUNT_DEBUG_NUMBER_OF_SYMBOLS]
-
-        # Use the function_map to apply the respective functions
-        # TODO: Change this implementation, its pretty unclear how its structured
-        result = {}
-        for field in field_name:
-            if field in map_field_name_to_function:
-                function_to_apply = map_field_name_to_function[field]
-                # TODO: What if these functions have different args? We shouldn't do this IMO
-                result[field] = function_to_apply(remote_symbols_from_index)
-
-        save_dataframe_to_arctic(
-            "equities", result["historic_price_full"], "adjusted_prices"
-        )
-        print("Saved Stock Level Data")
+def get_all_stock_symbols(symbol):
+    all_symbols = fmp_data.gather_symbols_from_index_no_survivorship(symbol)
+    return all_symbols
 
 
-def get_index_level_data():
-    # Run for all elements of the schedule
-    for field_name, index_list in index_level_data_mapping.items():
-        index_symbols = [
-            index_ticker
-            for readable_index_symbol, index_ticker in index_level_symbol_mapping.items()
-            if readable_index_symbol in index_list
-        ]
+def get_many_stock_prices(symbols):
+    all_prices = fmp_data.gather_simple_prices(symbols)
+    return all_prices
 
-        # Use the function_map to apply the respective functions
-        result = {}
-
-        if field_name in map_field_name_to_function:
-            function_to_apply = map_field_name_to_function[field_name]
-            # TODO: What if these functions have different args? We shouldn't do this IMO
-            result[field_name] = function_to_apply(index_symbols)
-
-        save_dataframe_to_arctic(
-            "equities", result["historic_price_full"], "adjusted_prices"
-        )
-        print("Saved Index Level Data")
 
 
 @click.command()
-@click.option(
-    "--data_save_path",
-    help="Pass path to save data to",
-    required=True,
-    default="data/data_cache",
-)
-def get_stock_and_index_data(data_save_path):
-    get_single_stock_data(data_save_path)
-    get_index_level_data(data_save_path)
+def get_stock_and_index_data():
+    spx_constituents = get_all_stock_symbols("sp500_constituent")
+    all_stock_prices = get_many_stock_prices(spx_constituents[:COUNT_DEBUG_NUMBER_OF_SYMBOLS])
+
+    # Save prices to arctic
+    arctic_store.save_dataframe_to_arctic(arctic_library_name="equities", data=all_stock_prices, symbol="equities")
 
 
 if __name__ == "__main__":
